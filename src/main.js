5b9bf73a70d7f8ed5baf0710256638301a76c6b4
Victor Sage <victorsage@Yanns-MacBook-Pro.local>
2019-06-25 23:23:44 +0200

    Changed to env vars

--- src/main.js
+++ src/main.js
@@ -2,10 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import firebase from 'firebase'
-import firebaseConfig from './serviceAccountKey.json'
+// import firebaseConfig from './serviceAccountKey.json'
 import VModal from 'vue-js-modal'
- 
-firebase.initializeApp(firebaseConfig)
+
+console.log(process.env)
+firebase.initializeApp({
+  "apiKey": process.env.apiKey || process.env.VUE_APP_apiKey,
+  "authDomain": process.env.authDomain || process.env.VUE_APP_authDomain,
+  "databaseURL": process.env.databaseURL || process.env.VUE_APP_databaseURL,
+  "projectId": process.env.projectId || process.env.VUE_APP_projectId,
+  "storageBucket": process.env.storageBucket || process.env.VUE_APP_storageBucket,
+  "messagingSenderId": process.env.messagingSenderId || process.env.VUE_APP_messagingSenderId,
+  "appId": process.env.appId || process.env.VUE_APP_appId
+})
+// firebase.initializeApp(firebaseConfig)
 
 Vue.config.productionTip = false
 Vue.use(VModal, { dynamic: true, injectModalsContainer: true })
--- src/scripts/importMissions.js
+++ src/scripts/importMissions.js
@@ -1,23 +1,31 @@
 const firebase = require('firebase')
-const firebaseConfig = require('../serviceAccountKey.json')
-firebase.initializeApp(firebaseConfig)
+// const firebaseConfig = require('../serviceAccountKey.json')
+firebase.initializeApp({
+    "apiKey": process.env.apiKey || process.env.VUE_APP_apiKey,
+    "authDomain": process.env.authDomain || process.env.VUE_APP_authDomain,
+    "databaseURL": process.env.databaseURL || process.env.VUE_APP_databaseURL,
+    "projectId": process.env.projectId || process.env.VUE_APP_projectId,
+    "storageBucket": process.env.storageBucket || process.env.VUE_APP_storageBucket,
+    "messagingSenderId": process.env.messagingSenderId || process.env.VUE_APP_messagingSenderId,
+    "appId": process.env.appId || process.env.VUE_APP_appId
+})
 const firestore = firebase.firestore();
 
 const csvFilePath = __dirname + '/missions.csv'
 const csv = require('csvtojson')
 
 csv()
-.fromFile(csvFilePath)
-.then(function (jsonObj) {
-    const settings = { timestampsInSnapshots: true };
-    firestore.settings(settings);
-    for (mission of jsonObj) {
-        firestore.collection('missions').doc().set(mission).then((res) => {
-            console.log("Document  successfully written!");
-        }).catch((error) => {
-            console.error("Error writing document: ", error);
-        });
-    }
-}, function (err) {
-    console.log(err)
-})
+    .fromFile(csvFilePath)
+    .then(function (jsonObj) {
+        const settings = { timestampsInSnapshots: true };
+        firestore.settings(settings);
+        for (mission of jsonObj) {
+            firestore.collection('missions').doc().set(mission).then((res) => {
+                console.log("Document  successfully written!");
+            }).catch((error) => {
+                console.error("Error writing document: ", error);
+            });
+        }
+    }, function (err) {
+        console.log(err)
+    })
